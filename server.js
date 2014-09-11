7d086e0cf911c3280e5dcce274e32a633d7b1463
Adrian Nita Nastac <nnadrian@gmail.com>
2014-09-11 15:29:38 +0300

    changes

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 var http = require('http');
 var path = require('path');
-http.createServer(function(request,response){
-	var lookup=path.basename(decodeURI(request.url));
-	response.writeHead(200, {'Content-Type': 'text/html'});
-	response.end('Woohoo!');
+var pages = [
+    {id: '1', route: '', output: 'Woohoo!'},
+    {id: '2', route: 'about', output: 'A simple routing with Node example'},
+    {id: '3', route: 'another page', output: function () {
+        return 'Here\'s ' + this.route; }
+    }
+];
+http.createServer(function (request, response) {
+    var http = require('http');
+    var url = require('url');
+    var id = url.parse(decodeURI(request.url), true).query.id;
+    if (id) {
+        pages.forEach(function (page) {
+            if (page.id === id) {
+                response.writeHead(200, {'Content-Type': 'text/html'});
+                response.end(typeof page.output === 'function'
+                    ? page.output() : page.output);
+            }
+        });
+    }
+    if (!response.finished) {
+        response.writeHead(404);
+        response.end('Page Not Found');
+    }
 }).listen(8080);
