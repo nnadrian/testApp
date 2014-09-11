a9077fe262ee24f6b1491f7a12d6d29b1236e1ef
Adrian Nita Nastac <nnadrian@gmail.com>
2014-09-11 18:22:05 +0300

    page 33/379 basic caching

--- server.js
+++ server.js
@@ -1,11 +1,45 @@
 var http = require('http');
 var path = require('path');
 var fs = require('fs');
+var mimeTypes = {
+    '.js' : 'text/javascript',
+    '.html': 'text/html',
+    '.css' : 'text/css'
+};
+
+var cache = {};
+function cacheAndDeliver(f, cb) {
+    if (!cache[f]) {
+        fs.readFile(f, function(err, data) {
+            if (!err) {
+                cache[f] = {content: data} ;
+            }
+            cb(err, data);
+        });
+        return;
+    }
+    console.log('loading ' + f + ' from cache');
+    cb(null, cache[f].content);
+}
 
 http.createServer(function (request, response) {
-    var lookup = path.basename(decodeURI(request.url)) || 'index.html';
+    var lookup = path.basename(decodeURI(request.url)) ||
+        'index.html';
     var f = 'content/' + lookup;
     fs.exists(f, function (exists) {
-        console.log(exists ? lookup + " is there" : lookup + " doesn't exist");
-    });
-}).listen(8000);
+            if (exists) {
+                cacheAndDeliver(f, function(err, data) {
+                    if (err) {
+                        response.writeHead(500);
+                        response.end('Server Error!');
+                        return; }
+                    var headers = {'Content-type': mimeTypes[path.extname(f)]};
+                    response.writeHead(200, headers);
+                    response.end(data);
+                });
+                return;
+            }
+            response.writeHead(404); //no such file found!
+            response.end();
+        });
+    }).listen(8000);
