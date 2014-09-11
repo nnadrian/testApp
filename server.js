ccf1e7a3a61642f63814d1828aeaf340aa32da13
Adrian Nita Nastac <nnadrian@gmail.com>
2014-09-11 17:58:31 +0300

    page 27/379

--- server.js
+++ server.js
@@ -1,27 +1,11 @@
 var http = require('http');
 var path = require('path');
-var pages = [
-    {id: '1', route: '', output: 'Woohoo!'},
-    {id: '2', route: 'about', output: 'A simple routing with Node example'},
-    {id: '3', route: 'another page', output: function () {
-        return 'Here\'s ' + this.route; }
-    }
-];
+var fs = require('fs');
+
 http.createServer(function (request, response) {
-    var http = require('http');
-    var url = require('url');
-    var id = url.parse(decodeURI(request.url), true).query.id;
-    if (id) {
-        pages.forEach(function (page) {
-            if (page.id === id) {
-                response.writeHead(200, {'Content-Type': 'text/html'});
-                response.end(typeof page.output === 'function'
-                    ? page.output() : page.output);
-            }
-        });
-    }
-    if (!response.finished) {
-        response.writeHead(404);
-        response.end('Page Not Found');
-    }
-}).listen(8080);
+    var lookup = path.basename(decodeURI(request.url)) || 'index.html';
+    var f = 'content/' + lookup;
+    fs.exists(f, function (exists) {
+        console.log(exists ? lookup + " is there" : lookup + " doesn't exist");
+    });
+}).listen(8000);
