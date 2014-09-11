085bb0bbf3481f8923666eea471e9ba845ef346b
Adrian Nita Nastac <nnadrian@gmail.com>
2014-09-11 17:14:05 +0300

    add client part too

---
+++ content/script.js
@@ -0,0 +1,3 @@
+/**
+ * Created by lenovo on 11/09/2014.
+ */
--- server.js
+++ server.js
@@ -25,3 +25,5 @@ http.createServer(function (request, response) {
         response.end('Page Not Found');
     }
 }).listen(8080);
+
+//window.onload = function() { alert('Yay Node!'); };
